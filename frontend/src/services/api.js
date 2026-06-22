95511bc4314d52784755e40574e573615b4c7978
Gulbeer Singh <gulbeersinghchauhan444@gmail.com>
2026-06-23 02:08:18 +0530

    Configure API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,21 +1,23 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://inventory-management-system-jusl.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 api.interceptors.request.use(
   (config) => {
-    const token =
-      localStorage.getItem("token");
+    const token = localStorage.getItem("token");
 
     if (token) {
-      config.headers.Authorization =
-        `Bearer ${token}`;
+      config.headers.Authorization = `Bearer ${token}`;
     }
 
     return config;
-  }
+  },
+  (error) => Promise.reject(error)
 );
 
 export default api;
