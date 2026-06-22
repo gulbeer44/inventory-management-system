b5e739d70a0f9453c05c025f0e2cfe02f6680eba
Gulbeer Singh <gulbeersinghchauhan444@gmail.com>
2026-06-23 00:47:26 +0530

    Add Vercel routing support

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://inventory-management-system-jusl.onrender.com",
 });
 
 api.interceptors.request.use(
